03e25e508a2c9a81ea2b87f5a41661209a0a58f3
yogniku <156093318+yogniku@users.noreply.github.com>
2024-02-19 21:25:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function toCase(text) {
   // write your code here
-	if (!text) return '-'; // Handle empty strings
+	 if (!text) return '-'; // Handle empty strings
     
-    const lowerCase = str.toLowerCase();
-    const upperCase = str.toUpperCase();
+    const lowerCase = text.toLowerCase();
+    const upperCase = text.toUpperCase();
 
-    return `${lowerCase}-${upperCase}`;
+    return `${lowerCase}-${upperCase};
+
 }
-console.log(toCase('Mthatha'));     
-console.log(toCase('HelloWorld'));   
-console.log(toCase('OpenAI'));       
-console.log(toCase(''));             
-console.log(toCase('My Name'));
+
+
 // DO not change the code below
 
 const text = prompt("Enter text:");
